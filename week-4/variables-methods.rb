a2337be431bb2f1762c59a2995b7f58aeaf20b6d
kristallam <kristal.lam24@gmail.com>
2016-03-01 10:31:42 -0800

    added number

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -6,3 +6,9 @@ puts "what is your last name?"
   last_name= gets.chomp
 
 puts 'Hello ' + first_name + ' ' + middle_name + ' ' + last_name
+
+puts "What is your favorite number?"
+  favorite_number= gets.chomp
+
+puts 1 + favorite_number.to_i
+puts "This is a bigger and better number"
