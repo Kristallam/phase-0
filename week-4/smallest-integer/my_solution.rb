d846b57dc67261064aa6695bde6e929327007949
kristallam <kristal.lam24@gmail.com>
2016-03-03 21:21:51 -0800

    4.6.1

--- week-4/smallest-integer/my_solution.rb
+++ week-4/smallest-integer/my_solution.rb
@@ -11,6 +11,16 @@
 # If +list_of_nums+ is empty the method should return nil
 
 # Your Solution Below
+# For this, I was thinking i=0 and while it goes through the list of numbers it will print i which is less than list of numbers. Then i += 1 until it ends. It didn't run, I timeboxed and moved on
+#def smallest_integer(list_of_nums)
+#  i = 0
+#  while i < list_of_nums[i]
+#    p i
+#  i += 1
+#  end
+#end
+# REFACTOR using ruby method
+# .MIN gives you the smallest number in the parameter
 def smallest_integer(list_of_nums)
    p list_of_nums.min
 end
