baca158dc20ab021ef5c0a9822f4d49b8341de13
kristallam <kristal.lam24@gmail.com>
2016-03-07 21:15:06 -0800

    reflection

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -12,10 +12,16 @@ my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, 
     #I used .map! then went through all the numbers and if it was an integer then added 1 to it
     # "!" will modify the array
   #Output: The array but all the integers within the array will have 1 added to it
+
 # Person 2
-#def my_array_modification_method!(i_want_pets, n)
-#  i_want_pets.map! {|n| n.is_a?(Integer) ? n + 1 : n}
-#end
+
+def my_array_modification_method!(i_want_pets, n)
+  i_want_pets.map! {|n| n.is_a?(Integer) ? n + 1 : n}
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+# I used for .map! - invokes the given block once for each element of self, replacing the element with the value returned by the block.
+# Also I said if n is an interger then n + 1
 
 def my_hash_modification_method!(my_family_pets_ages, n)
   my_family_pets_ages.map {|k,v| [k, v + 2]}.to_h
@@ -23,22 +29,21 @@ end
 
 
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+# it goes through the key and value of my_family_pets_ages and then it will add 2 to each value
+# then turn it back to a hash with .to_h
 
 
 
 
 # Release 1: Identify and describe the Ruby method you implemented. Teach your
 # accountability group how to use the methods.
-#
+# Look under the code for instructions on what I did
 #
 #
 
 
 # Release 3: Reflect!
 # What did you learn about researching and explaining your research to others?
-#
+# This one was hard for me because I really had no idea on how to do it. So I did a lot of researching and was able to find out how to add the numbers. It was easier for the Array and much harder to do for the Hash
 #
 #
