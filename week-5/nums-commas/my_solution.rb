c58ffc21869e6978a9562bbe0fc02b28f69f50ae
kristallam <kristal.lam24@gmail.com>
2016-03-08 21:45:57 -0800

    code v1

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -8,19 +8,46 @@
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? Positive Integer
+# What is the output? It will return a String of comma seperated integers
 # What are the steps needed to solve the problem?
+  # Need to go through the integer and seperate it into commas
+  # Change it to a string
+  # Return the out as a string with commas
 
 
 # 1. Initial Solution
+def separate_comma(number)
+  num = number.to_s.split)
+    # number be turned into string and the split it up
+  new_num = num.size/3
+    # the num variable be divded by 3 because thats where the commas will go
+  if num.size < 4
+    # if the number is less than 4 it will ...
+    p number.to_string
+  elsif num.size%3 == 0
+    new_num.times do |i|
+  end
+end
+  p number
+end
 
 
 
-
+=begin
 # 2. Refactored Solution
+def separate_comma(number)
+  number.to_s.chars.to_a.reverse.each_slice(3).join(",").reverse
+end
 
-
+#The code won't run saying undefined method join, my thought process of what each meant is below. I told it to with the comma.
+#.to_s makes it a string
+#.chars returns an array of characters in str, ruby doc(rubyist pg.308 )
+#.to_a converts the object to an array
+#.reverse reserve self in place
+#.each_slice(3) walk through a number of certain elements at a time, for this one 3
+#joined it with the comma
 
 
 # 3. Reflection
+=end
