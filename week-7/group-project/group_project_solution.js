7f903a2ea42e71a0491b5cdf584debb0517f55c8
kristallam <kristal.lam24@gmail.com>
2016-03-21 21:29:14 -0700

    Added User Stories

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,25 @@
+/*
+User Stories by Person 1
+
+*Test For Sum*
+1. As a user, I want to create a function called sum
+
+2. As a user, I want the function sum to determine the sum of all elments in a list, even if the list has an odd number of elements
+
+3. As a user, I want the function sum to determine the sum of all elments in a list, even if the list has an even number of elements
+
+*Test For Mean*
+4. As a user, I want to create a function called mean
+
+5. As a user, I want to have the function mean determine the average of all elements in the list, even if list has an odd number of elements
+
+6. As a user, I want to have the function mean determine the average of all elements in the list, even if list has an even number of elements
+
+*Test For Median*
+7. As a user, I want to create a function called median
+
+8. As a user, I want to have the function median determine the median value of all elements within the list, even if the list has an odd number of elements
+
+9.  As a user, I want to have the function median determine the median value of all elements within the list, even if the list has an even number of elements
+
+*/
