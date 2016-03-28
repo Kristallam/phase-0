cf66eeea3f3bacdde8be584912f6f68eab233e26
kristallam <kristal.lam24@gmail.com>
2016-03-27 20:49:01 -0700

    group work

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -23,3 +23,61 @@ User Stories by Person 1
 9.  As a user, I want to have the function median determine the median value of all elements within the list, even if the list has an even number of elements
 
 */
+
+/*
+Pseudocode by Person 2 (Samantha Holmes)
+*Test For Sum*
+  - define a function "sum" that takes in one argument: an array
+  - add all elements in the array (list)
+    - set it equal to a variable "sum"
+  - return variable "sum"
+  - end function "sum"
+*Test For Mean*
+  - define a function "mean" that take in one argument: an array
+  - add all elements in the array (list)
+    - divide this value by the length of the array
+    - set this equal to a variable "mean"
+  - return variable "mean"
+  - end function "mean"
+*Test For Median*
+  - define a function "median" that takes in one argument: an array
+  - check if the length of the array is an odd number or an even number
+    - IF the length is odd
+      - take the element at the index equal to the length divided by 2 (using integer division)
+        - return this equal to a variable "median"
+    - ELSE
+      - take the element at the index equal to the length divided by 2
+        - add this value to the element at the index equal to (the length divided by 2) minus 1
+        - divide the summed value by 2 (using float division)
+        - return this equal to a variable "median"
+    - end IF/ELSE statement
+  -end function "median"
+*/
+
+// JavaScript from Pseudocode; Person 3 (John Colella and Elizabeth Brown)
+
+var sum = function(array) {
+  var sum = 0
+  for (var index = 0; index < array.length; index++) {
+    sum += array[index]
+  };
+  return sum
+};
+
+var mean = function(array) {
+  var sum = 0
+  for (var index = 0; index < array.length; index++) {
+    sum += array[index]
+  };
+  var mean = sum / array.length
+  return mean
+};
+
+var median = function(array) {
+  if (array.length % 2 != 0)
+    var median = array[(array.length - 1) / 2];
+  else
+    var median = (array[array.length / 2] + array[array.length / 2 - 1]) / 2.0;
+  console.log(median);
+  return median;
+};
